5688ba87a154dcaf6ca56fb5d826ff7539411ad4
abidi-muhammad <abidimuhammad345@gmail.com>
2026-01-16 21:47:12 +0500

    Add missing index.css

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/qiam/',  // ← IMPORTANT! Your subdirectory
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
