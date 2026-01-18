39abd8f21ac543c914b59767efa22be72de1cb16
abidi-muhammad <abidimuhammad345@gmail.com>
2026-01-18 13:17:01 +0500

    v4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/QiamConsulting/',  // ← MUST match your repository name EXACTLY
+  base: '/',  // ← MUST match your repository name EXACTLY
   plugins: [react()],
   resolve: {
     alias: {
